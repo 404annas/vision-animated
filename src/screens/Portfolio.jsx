f123e106cee607bd23b4ce4200fa540d1ef6e5f6
Muhammad Annas Khan <annasking601@gmail.com>
2025-12-18 17:49:27 +0500

    Changes

--- src/screens/Portfolio.jsx
+++ src/screens/Portfolio.jsx
@@ -63,7 +63,6 @@ const Portfolio = () => {
                 </p>
             </div>
 
-            {/* Grid */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 {images.map((item, index) => (
                     <Link
